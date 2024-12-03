6250ee2d46995839e2aa9c8cbca844f49abe2e69
FaisyalRachman <faisal.paguntaka@gmail.com>
2024-12-03 10:21:36 +0800

    ubah link

--- application/views/bracatheme/footer.php
+++ application/views/bracatheme/footer.php
@@ -3,7 +3,7 @@
     <div class="col-lg-2" style="padding-top:15px"><a href="<?php echo base_url('treatment'); ?>">Treatment</a></div>
     <div class="col-lg-2" style="padding-top:15px"><a href="<?php echo base_url('lenyskincaretips'); ?>">Berita</a></div>
     <div class="col-lg-2" style="padding-top:15px"><a href="<?php echo base_url('karir'); ?>">Karir</a></div>
-    <div class="col-lg-2" style="padding-top:15px"><a href="<?php echo base_url('hubungi');?>">Hubungi Kami</a></div>
+    <div class="col-lg-2" style="padding-top:15px"><a href="<?php echo base_url('hubungi_klinik_kecantikan_tangerang');?>">Hubungi Kami</a></div>
     <div class="col-lg-2 trademark"><a href="<?php echo $pecahd[0]; ?>"><i class="bx bxl-instagram" style="font-size:1.9em"></i></a> <a href="<?php echo $pecahd[1]; ?>"><i class="bx bxl-facebook" style="font-size:1.9em"></i></a> <a href="<?php echo $pecahd[2]; ?>"><i class="bx bxl-tiktok" style="font-size:1.9em"></i></a><a href="<?php echo $pecahd[3]; ?>"> <i class="bx bxl-youtube" style="font-size:1.9em"></i></a><div style="font-size:0.8em;font-family:verdana;"><i>powered by elokreatif</i></div></div>
       </div>
   
