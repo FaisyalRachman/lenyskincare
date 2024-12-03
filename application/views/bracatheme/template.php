4117d60efc307cbc11904a719634320c4d1e118c
FaisyalRachman <faisal.paguntaka@gmail.com>
2024-12-03 11:30:21 +0800

    tambah meta tag dan nonaktif preload

--- application/views/bracatheme/template.php
+++ application/views/bracatheme/template.php
@@ -3,8 +3,12 @@
 
 <head>
   <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
+  <title><?= $title ?></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" /> 
+  <meta name="description" content="<?= $description ?>" />
+  <meta name="keywords" content="<?= $keywords ?>" />
+  <meta name="title" content="<?= $title ?>">
   <meta property="og:title" content="<?= $title ?>"> 
   <meta property="og:description" content="<?= $description ?>">
   <meta property="og:keyword" content="<?= $keywords ?>">
