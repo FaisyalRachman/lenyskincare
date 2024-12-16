b707ba2496cd9e332517c0ee1fbfcfc5216bb426
FaisyalRachman <faisal.paguntaka@gmail.com>
2024-12-16 23:07:55 +0800

    ubah tampilan list berita tambahkan label penulis

--- application/views/administrator/mod_berita/view_berita.php
+++ application/views/administrator/mod_berita/view_berita.php
@@ -12,7 +12,7 @@
         <tr>
           <th style='width:40px'>No</th>
           <th>Judul Artikel</th>
-          <th>Tanggal</th>
+          <th>Kategori</th>
           <th>Tag</th>
           <th>Status</th>
           <th style='width:75px'>Action</th>
@@ -20,15 +20,19 @@
       </thead>
       <tbody>
       <?php 
+    
         $no = 1;
         foreach ($record as $row){
+          if ($this->session->level == 'admin') {
+            $hps = base_url().$this->uri->segment(1)."/delete_listberita/$row[id_berita]";
+          }
         $tgl_posting = tgl_indo($row['tanggal']);
         if ($row['status']=='Y'){ $status = '<span style="color:green">Published</span>'; }else{ $status = '<span style="color:red">Unpublished</span>'; }
-        echo "<tr><td>$no</td>
-                  <td>$row[judul]</td>
-                  <td>$tgl_posting</td>
-                  <td>$row[tag]</td>
-                  <td>$status</td>
+        echo "<tr><td><small>$no.</small></td>
+                  <td><small><b>$row[judul]</b><br><i>Penulis : $row[nama_lengkap] - $tgl_posting</i><br>Meta Description : $row[meta_desc]</small></td>
+                  <td><small>$row[nama_kategori]</small></td>
+                  <td><small>$row[tag]</small></td>
+                  <td><small>$status</small></td>
                   <td><center>
                     <a class='btn btn-primary btn-xs' title='Publish Data' href='".base_url().$this->uri->segment(1)."/publish_listberita/$row[id_berita]/$row[status]'><span class='nav-icon fas fa-check-double'></span></a>
                     <a class='btn btn-success btn-xs' title='Edit Data' href='".base_url().$this->uri->segment(1)."/edit_listberita/$row[id_berita]'><span class='nav-icon fas fa-edit'></span></a>
