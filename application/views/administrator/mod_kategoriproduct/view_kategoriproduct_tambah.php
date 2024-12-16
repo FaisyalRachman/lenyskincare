7f64d57918c33350e5d2483fd5920bb0393ac967
FaisyalRachman <faisal.paguntaka@gmail.com>
2024-12-17 07:47:11 +0800

    input awal field form untuk input meta tag

--- application/views/administrator/mod_kategoriproduct/view_kategoriproduct_tambah.php
+++ application/views/administrator/mod_kategoriproduct/view_kategoriproduct_tambah.php
@@ -14,6 +14,8 @@
                     <tr><th width='120px' scope='row'>Nama Produk</th>    <td><input type='text' class='form-control' name='a' required></td></tr>
                     <tr><th width='120px' scope='row'>What's Include</th>    <td><textarea name='e' rows='10' cols='90'></textarea></td></tr>
                     <tr><th width='120px' scope='row'>Term & Condition</th>    <td><textarea name='f' rows='10' cols='90'></textarea></td></tr>
+                      <tr><th width='220px' scope='row'>Meta Title</th>    <td><input type='text' class='form-control' name='m' placeholder='Contoh : Ttreatment IPL Rejuve' value='$rows[meta_title]' required></td></tr></td></tr>
+                    <tr><th width='220px' scope='row'>Meta Description</th>    <td><textarea class='form-control' name='n' placeholder='Contoh : Ingin menjaga kekencangan kulit tanpa operasi? Yuk coba treatment IPL Rejuve! Baca di LenySkinCare' value=''required>$rows[meta_desc]</textarea></td></tr>
                     <tr><th scope='row'>Promo</th>                          <td>"; if ($rows['aktif']=='Y'){ echo "<input type='radio' name='b' value='Y' checked> Ya &nbsp; <input type='radio' name='b' value='N'> Tidak"; }else{ echo "<input type='radio' name='b' value='Y'> Ya &nbsp; <input type='radio' name='b' value='N' checked> Tidak"; } echo "</td></tr>
                      <tr><td> <tr><td><input type='hidden' class='form-control' name='c' value='2' placeholder='Treatment' readonly></td></tr></td></tr>
                     <tr><th width='120px' scope='row'>Gambar</th>    <td><input type='file' class='form-control' name='d'><small>Ukuran gambar * 1280 x 537</small></td></tr>
